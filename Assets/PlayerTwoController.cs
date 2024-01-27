d35328347810f690dd1fa96c39774a77da076ba0
cozzie4d <cris@sierracosworth.nl>
2024-01-27 13:55:43 +0100

    two player controls and horse sprites added

--- Assets/PlayerTwoController.cs
+++ Assets/PlayerTwoController.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 
 public class PlayerTwoController : MonoBehaviour
 {
-    private Vector2 currMovement;
-
-    private Rigidbody rb;
+    // Movement properties
     public float moveSpeed;
+
+    private bool LTpressedLastUpdate = false;
+    private bool LSpressedLastUpdate = false;
+    private bool RTpressedLastUpdate = false;
+    private bool RSpressedLastUpdate = false;
+
+    private bool LTpressedCurrUpdate = false;
+    private bool LSpressedCurrUpdate = false;
+    private bool RTpressedCurrUpdate = false;
+    private bool RSpressedCurrUpdate = false;
+
+    // Phyics/logics
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +30,49 @@ public class PlayerTwoController : MonoBehaviour
     // Fixed update for physics/ fixed timestep
     void FixedUpdate()
     {
-        //Debug.Log("P2 FixedUpdate executed, movement: " + currMovement);
-
-        float moveX = Input.GetAxis("HorizontalB");
+        /// OLD: Move using Left Stick
+ /*       float moveX = Input.GetAxis("HorizontalB");
         float moveY = Input.GetAxis("VerticalB");
-        //Vector3 movement = new Vector3(moveX, 0.0f, moveY);
+        Vector3 movement = new Vector3(moveX, 0.0f, 0.0f);
+        rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));*/
 
-        transform.position = transform.position + new Vector3(moveX, 0.0f, 0.0f);
+        // Move forward right trigger + right shoulder
+        // Move backward left trigger + left shoulder
+        LTpressedCurrUpdate = Input.GetAxis("PlayerTwoLT") == -1;
+        LSpressedCurrUpdate = Input.GetButton("PlayerTwoLS");
+
+        RTpressedCurrUpdate = Input.GetAxis("PlayerTwoRT") == -1;
+        RSpressedCurrUpdate = Input.GetButton("PlayerTwoRS");
+
+        // FORWARD
+        if (RTpressedLastUpdate && RSpressedCurrUpdate)
+        {
+            Vector3 movement = new Vector3(1.0f, 0.0f, 0.0f);
+            rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));
+        }
+        else if (RSpressedLastUpdate && RTpressedCurrUpdate)
+        {
+            Vector3 movement = new Vector3(1.0f, 0.0f, 0.0f);
+            rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));
+        }
+
+        // BACKWARD
+        if (LTpressedLastUpdate && LSpressedCurrUpdate)
+        {
+            Vector3 movement = new Vector3(-1.0f, 0.0f, 0.0f);
+            rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));
+        }
+        else if (LSpressedLastUpdate && LTpressedCurrUpdate)
+        {
+            Vector3 movement = new Vector3(-1.0f, 0.0f, 0.0f);
+            rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));
+        }
+
+        LTpressedLastUpdate = LTpressedCurrUpdate;
+        LSpressedLastUpdate = LSpressedCurrUpdate;
+
+        RTpressedLastUpdate = RTpressedCurrUpdate;
+        RSpressedLastUpdate = RSpressedCurrUpdate;
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerOneController.cs
+++ Assets/Scripts/PlayerOneController.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 
 public class PlayerOneController : MonoBehaviour
 {
-    private Vector2 currMovement;
-
-    private Rigidbody rb;
+    // Movement properties
     public float moveSpeed;
+
+    private bool LTpressedLastUpdate = false;
+    private bool LSpressedLastUpdate = false;
+    private bool RTpressedLastUpdate = false;
+    private bool RSpressedLastUpdate = false;
+
+    private bool LTpressedCurrUpdate = false;
+    private bool LSpressedCurrUpdate = false;
+    private bool RTpressedCurrUpdate = false;
+    private bool RSpressedCurrUpdate = false;
+
+    public AudioSource walkSound1;
+    public AudioSource walkSound2;
+
+    // Phyics/logics
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
@@ -19,15 +33,57 @@ public class PlayerOneController : MonoBehaviour
     // Fixed update for physics/ fixed timestep
     void FixedUpdate()
     {
-        //Debug.Log("P1 FixedUpdate executed, movement: " + currMovement);
-
-        float moveX = Input.GetAxis("Horizontal");
+        /// OLD: Move using Left Stick
+        /*float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveX, 0.0f, moveY);
+        Vector3 movement = new Vector3(moveX, 0.0f, 0.0f);
+        rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));*/
 
-        transform.position = transform.position + new Vector3(moveX, 0.0f, 0.0f);
+        // Move forward right trigger + right shoulder
+        // Move backward left trigger + left shoulder
+        LTpressedCurrUpdate = Input.GetAxis("PlayerOneLT") == -1;
+        LSpressedCurrUpdate = Input.GetButton("PlayerOneLS");
 
-//        rb.AddForce(movement * moveSpeed);        
+        RTpressedCurrUpdate = Input.GetAxis("PlayerOneRT") == -1;
+        RSpressedCurrUpdate = Input.GetButton("PlayerOneRS");
+
+        // FORWARD
+        if (RTpressedLastUpdate && RSpressedCurrUpdate)
+        {
+            Vector3 movement = new Vector3(1.0f, 0.0f, 0.0f);
+            rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));
+
+            walkSound1.Play();
+        }
+        else if (RSpressedLastUpdate && RTpressedCurrUpdate)
+        {
+            Vector3 movement = new Vector3(1.0f, 0.0f, 0.0f);
+            rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));
+
+            walkSound2.Play();
+        }
+
+        // BACKWARD
+        if (LTpressedLastUpdate && LSpressedCurrUpdate)
+        {
+            Vector3 movement = new Vector3(-1.0f, 0.0f, 0.0f);
+            rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));
+
+            walkSound1.Play();
+        }
+        else if (LSpressedLastUpdate && LTpressedCurrUpdate)
+        {
+            Vector3 movement = new Vector3(-1.0f, 0.0f, 0.0f);
+            rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));
+
+            walkSound2.Play();
+        }
+
+        LTpressedLastUpdate = LTpressedCurrUpdate;
+        LSpressedLastUpdate = LSpressedCurrUpdate;
+
+        RTpressedLastUpdate = RTpressedCurrUpdate;
+        RSpressedLastUpdate = RSpressedCurrUpdate;
     }
 
     // Update is called once per frame
