1b5cd2eba52a447433465fdfb92fc62e1949b4b1
cozzie4d <cris@sierracosworth.nl>
2024-01-27 14:36:58 +0100

    Player controllers fixed, one script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.InputSystem;
 using UnityEngine;
 
-public class PlayerOneController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Movement properties
     public float moveSpeed;
@@ -18,8 +17,10 @@ public class PlayerOneController : MonoBehaviour
     private bool RTpressedCurrUpdate = false;
     private bool RSpressedCurrUpdate = false;
 
-    public AudioSource walkSound1;
-    public AudioSource walkSound2;
+    public string InputWalkLeftA;
+    public string InputWalkLeftB;
+    public string InputWalkRightA;
+    public string InputWalkRightB;
 
     // Phyics/logics
     private Rigidbody rb;
@@ -28,39 +29,36 @@ public class PlayerOneController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        string[] controllerNames;
+        controllerNames = Input.GetJoystickNames();
+
+        Debug.Log("Joystick name: " + controllerNames[0]);
+        Debug.Log("Joystick name: " + controllerNames[1]);
+        Debug.Log("Joystick name: " + controllerNames[2]);
     }
 
     // Fixed update for physics/ fixed timestep
     void FixedUpdate()
     {
-        /// OLD: Move using Left Stick
-        /*float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveX, 0.0f, 0.0f);
-        rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));*/
-
         // Move forward right trigger + right shoulder
         // Move backward left trigger + left shoulder
-        LTpressedCurrUpdate = Input.GetAxis("PlayerOneLT") == -1;
-        LSpressedCurrUpdate = Input.GetButton("PlayerOneLS");
+        LTpressedCurrUpdate = Input.GetAxis(InputWalkLeftA) == -1;
+        LSpressedCurrUpdate = Input.GetButton(InputWalkLeftB);
 
-        RTpressedCurrUpdate = Input.GetAxis("PlayerOneRT") == -1;
-        RSpressedCurrUpdate = Input.GetButton("PlayerOneRS");
+        RTpressedCurrUpdate = Input.GetAxis(InputWalkRightA) == -1;
+        RSpressedCurrUpdate = Input.GetButton(InputWalkRightB);
 
         // FORWARD
         if (RTpressedLastUpdate && RSpressedCurrUpdate)
         {
             Vector3 movement = new Vector3(1.0f, 0.0f, 0.0f);
             rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));
-
-            walkSound1.Play();
         }
         else if (RSpressedLastUpdate && RTpressedCurrUpdate)
         {
             Vector3 movement = new Vector3(1.0f, 0.0f, 0.0f);
             rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));
-
-            walkSound2.Play();
         }
 
         // BACKWARD
@@ -68,15 +66,11 @@ public class PlayerOneController : MonoBehaviour
         {
             Vector3 movement = new Vector3(-1.0f, 0.0f, 0.0f);
             rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));
-
-            walkSound1.Play();
         }
         else if (LSpressedLastUpdate && LTpressedCurrUpdate)
         {
             Vector3 movement = new Vector3(-1.0f, 0.0f, 0.0f);
             rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));
-
-            walkSound2.Play();
         }
 
         LTpressedLastUpdate = LTpressedCurrUpdate;
